aecce0f7b3b0555b0c194d3e54b68ac3a019af9c
Shyam Amol Lade <shyamlade06@gmail.com>
2025-10-29 22:08:25 +0530

    Add DataTypes1.java to display data type sizes

---
+++ DataTypes1.java
@@ -0,0 +1,13 @@
+public class Btyes {
+	public static void main(String[] args) {
+		System.out.println("byte size is " + Byte.BYTES + " byte and in bits " + Byte.SIZE + " bits");
+		System.out.println("short size is " + Short.BYTES + " byte and in bits " + Short.SIZE+" bits");
+		System.out.println("int size is " + Integer.BYTES + " byte and in bits "+ Integer.SIZE+" bits");
+		System.out.println("long size is " + Long.BYTES +" byte and in bits " + Long.SIZE+" bits");
+		System.out.println("float size is " + Float.BYTES + " byte and in bits " + Float.SIZE+" bits");
+		System.out.println("double size is " + Double.BYTES + " byte and in bits " + Double.SIZE+" bits");
+		System.out.println("char size is " + Character.BYTES + " byte and in bits " + Character.SIZE+" bits");
+		System.out.println("boolean size is 1 bits");
+	}
+
+}
