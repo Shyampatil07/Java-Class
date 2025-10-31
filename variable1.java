d704f482571a6efb9ac099f13ed0b6af03401b67
Shyam Amol Lade <shyamlade06@gmail.com>
2025-10-31 16:08:15 +0530

    Create variable1.java

---
+++ variable1.java
@@ -0,0 +1,22 @@
+
+public class SelfInfo {
+
+	public static void main(String[] args) {
+
+		String my_Name = "Shyam";
+    String panCard = "625 764 76Yf";
+    long AdharCard = 2354 6252 8253l;
+    String driving = "9854 9764 865RT";
+    long AccNo = 976687598398l;
+    int myAge = 24;
+		int rollNo = 123;
+		String email = "Shyam007@gmail.com";
+		float percent = 99.56f;
+		boolean isPassed = true;             
+    
+
+		System.out.println(my_Name);
+		
+	}
+
+}
