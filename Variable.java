e3b76cd2524a6b5325cc8734f082e6e5b3c2bb62
Shyam Amol Lade <shyamlade06@gmail.com>
2025-10-31 16:00:39 +0530

    Add Variable class with employee details

---
+++ Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable {
+	
+	public static void main(String[] args) {
+
+		String Empo_Name = "shyam";
+		int Age = 24;
+		int EmpID = 123;
+		String email = "shyam07@gmail.com";
+		float salary = 78000.00f;
+    String gender = "Male";
+		String address = "B building, floor no 4, 402";
+    String department = "Software developer";
+    
+
+		System.out.println(Empo_Name);
+		
+	}
+
+}
